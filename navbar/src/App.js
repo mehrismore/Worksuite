7c0f15d553bda50573e4791b27e3e1eb6c9523ac
Mehrnoosh Beniss <mehrnoosh.beniss@gmail.com>
2021-10-20 19:18:35 +0200

    feat: scaffolding, tailwind and basic navbar

---
+++ navbar/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MainNavbar } from "./components/MainNavbar";
+
+export const App = () => {
+  return (
+    <div>
+      <MainNavbar />
+      Home
+    </div>
+  );
+};
